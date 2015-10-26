5ee35e0e838c870a04b0cb301b32eedb89abbaf4
Scientifik <kurtdusek@gmail.com>
2015-10-25 20:23:52 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# SCLog_C-
+SCLog
+======
+
+SCLog is an OpenSource logging class intended to be used with the Oracle Service Cloud product.  
+
+### Highlights
+- Easy Install 
+- Configurable via Service Cloud console
+- Output logs to database or file system
+- Display file system logs via web browser
+
+
+### Custom Object File
+scLog_object.zip is included with this repository to be imported into your Service Cloud Instance
+    
+### Sample Code - C# #
+    using scTools.scLog;
+    ...
+    scLog logger = new scTools.scLog();
+    logger.initializeLogger(this._globalContext);
+    logger.log("A notice message to be logged", null, null, scLog.logLevel.Notice);
+    
+### Support
+	This add-in was developed by Service Ventures LLC (servicecloudedge.com) for more information email us info@servicecloudedge.com
---
+++ src/scLog.cs
@@ -0,0 +1,313 @@
+﻿/**
+ * SCLog is a logging class that groups output from several endpoints under a single file tree or database
+ * grouping for a specific Product Extension (customization). The idea is to make it easy to see what Product Extension
+ * exist on a site, which files comprise a given Product Extension and what they are doing as a system regardless of
+ * what the pieces are; Add-ins, CP controllers, CPMs, Custom Scripts, etc.
+ *
+ * SCLog was created by Service Ventures LLC. (servicecloudedge.com) and is being released under an MIT License.
+ *
+ * @copyright   (c) Service Ventures LLC
+ * @license     MIT
+ * @package     ServiceVentures
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using RightNow.AddIns.AddInViews;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SCLog.RightNowServiceReference;
+
+namespace ServiceVentures
+{
+    public class SCLog
+    {
+        public enum logLevel
+        {
+            Fatal = 1,
+            Error = 2,
+            Warning = 3,
+            Notice = 4,
+            Debug = 5,
+            Click = 6,
+            None = 0
+        }
+
+        #region private properties
+        private const string DEFAULT_EXT = "General";
+        private const string DEFAULT_SIG = "92acaed5cb6dc95783ea1d0b194347c542017bfc";
+        private const string DEFAULT_CONFIG = "{"
+                            + "\"logPessimistic\": \"0\","
+                            + "\"logtoFile\": \"1\","
+                            + "\"logtoDatabase\": \"1\","
+                            + "\"logThreshHold\": \"6\","
+                            + "\"logClicks\": \"1\","
+                            + "\"pruningReport\": \"Automatic scLog Pruning\""
+                            + "}";
+
+        private string extName;
+        private string extSignature;
+        private RNObject extObject;
+        private DateTime startTime;
+        private string functionServed;
+        private string callingFile;
+        private Boolean callerHadErrors;
+        private logLevel stdOutputThreshold;
+        private extConfig extConfigs;
+        private RightNowSyncPortClient client;
+        #endregion private properties
+
+        public SCLog(
+            string extensionName = DEFAULT_EXT,
+            string extensionSignature = DEFAULT_SIG,
+            DateTime startTime = new DateTime(),
+            logLevel stdOutLevel = logLevel.None,
+            string functionServed = "Not Specified",
+            string callingFile = "Not Specified"
+            )
+        {
+            this.extName = extensionName;
+            this.extSignature = extensionSignature;
+            this.startTime = startTime;
+            this.stdOutputThreshold = stdOutLevel;
+            this.functionServed = functionServed;
+            this.callingFile = callingFile;
+        }
+
+        public Boolean log(string message, string detail = null, Incident incident = null, Contact contact = null, string source = null, string function = null, int timeElapsed = 0, logLevel messageType = logLevel.Debug)
+        {
+            if (this.extConfigs == null)
+            {
+                throw new Exception("SCLog has not been initialized");
+            }
+            if ((int)messageType > this.extConfigs.logThreshhold)
+            {
+                return false;
+            }
+            GenericObject go = new GenericObject
+            {
+                ObjectType = new RNObjectType
+                {
+                    Namespace = "SvcVentures", // Custom Object
+                    TypeName = "scLog" // Custom Object
+                }
+            };
+            List<GenericField> fields = new List<GenericField>();
+            fields.Add(createGenericField("Message", ItemsChoiceType.StringValue, message));
+            fields.Add(createGenericField("MsgType", ItemsChoiceType.IntegerValue, (int)messageType));
+            long memory = GC.GetTotalMemory(true) / (1024 * 1024);
+            fields.Add(createGenericField("PeakMemory", ItemsChoiceType.IntegerValue, (int)memory));
+
+            if (string.IsNullOrWhiteSpace(source) == false)
+                fields.Add(createGenericField("File", ItemsChoiceType.StringValue, source));
+
+            if (string.IsNullOrWhiteSpace(function) == false)
+                fields.Add(createGenericField("Function", ItemsChoiceType.StringValue, function));
+
+            if (string.IsNullOrWhiteSpace(detail) == false)
+                fields.Add(createGenericField("Detail", ItemsChoiceType.StringValue, detail));
+
+            fields.Add(createGenericField("TimeElapsed", ItemsChoiceType.IntegerValue, timeElapsed));
+            fields.Add(createGenericField("scProductExtension", ItemsChoiceType.NamedIDValue,
+                new NamedID
+                {
+                    ID = new ID
+                    {
+                        id = (int)this.extObject.ID.id,
+                        idSpecified = true
+                    }
+                }
+            ));
+
+            if (!string.IsNullOrWhiteSpace(detail))
+                fields.Add(createGenericField("Detail", ItemsChoiceType.StringValue, detail));
+
+            go.GenericFields = fields.ToArray();
+
+            CreateProcessingOptions cpo = new CreateProcessingOptions();
+            cpo.SuppressExternalEvents = false;
+            cpo.SuppressRules = false;
+
+            ClientInfoHeader clientInfoHeader = new ClientInfoHeader();
+            clientInfoHeader.AppID = "Insert log";
+            RNObject[] results = this.client.Create(clientInfoHeader, new RNObject[] { go }, cpo);
+
+            // check result and save incident id
+            if (results != null && results.Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean error(string message, string detail = null, Incident incident = null, Contact contact = null, string source = null, string function = null, int timeElapsed = 0)
+        {
+            return this.log(message, detail, incident, contact, source, function, timeElapsed, logLevel.Error);
+        }
+
+        public Boolean debug(string message, string detail = null, Incident incident = null, Contact contact = null, string source = null, string function = null, int timeElapsed = 0)
+        {
+            return this.log(message, detail, incident, contact, source, function, timeElapsed, logLevel.Debug);
+        }
+
+        public Boolean notice(string message, string detail = null, Incident incident = null, Contact contact = null, string source = null, string function = null, int timeElapsed = 0)
+        {
+            return this.log(message, detail, incident, contact, source, function, timeElapsed, logLevel.Notice);
+        }
+
+        public Boolean initializeLogger(IGlobalContext globalContext)
+        {
+            EndpointAddress endPointAddr = new EndpointAddress(globalContext.GetInterfaceServiceUrl(ConnectServiceType.Soap));
+
+            // Minimum required
+            BasicHttpBinding binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportWithMessageCredential);
+            binding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.UserName;
+
+            // Optional depending upon use cases
+            binding.MaxReceivedMessageSize = 1024 * 1024;
+            binding.MaxBufferSize = 1024 * 1024;
+            binding.MessageEncoding = WSMessageEncoding.Mtom;
+
+            // Create client proxy class
+            RightNowSyncPortClient client = new RightNowSyncPortClient(binding, endPointAddr);
+
+            // Ask the client to not send the timestamp
+            BindingElementCollection elements = client.Endpoint.Binding.CreateBindingElements();
+            elements.Find<SecurityBindingElement>().IncludeTimestamp = false;
+            client.Endpoint.Binding = new CustomBinding(elements);
+
+            // Ask the Add-In framework the handle the session logic
+            globalContext.PrepareConnectSession(client.ChannelFactory);
+
+            this.client = client;
+            this.extObject = getExtension();
+            return true;
+        }
+
+        public Boolean initializeLogger(RightNowSyncPortClient client)
+        {
+            // Ask the client to not send the timestamp
+            BindingElementCollection elements = client.Endpoint.Binding.CreateBindingElements();
+            elements.Find<SecurityBindingElement>().IncludeTimestamp = false;
+            client.Endpoint.Binding = new CustomBinding(elements);
+
+            this.client = client;
+            this.extObject = getExtension();
+            return true;
+        }
+
+        protected RNObject getExtension()
+        {
+            RNObject currentExt = lookupExtension();
+            if (currentExt == null)
+            {
+                currentExt = createExtension();
+            }
+            GenericObject extentionObj = (GenericObject)currentExt;
+            string extConfigJson = extentionObj.GenericFields.FirstOrDefault<GenericField>(c => c.name == "ExtConfiguration").DataValue.Items[0].ToString();
+            this.extConfigs = JsonConvert.DeserializeObject<extConfig>(extConfigJson);
+            //var serializer = new JavaScriptSerializer(); - use this instead of newtonsoft if you prefer
+            //this.extConfigs = serializer.Deserialize<extConfig>(extConfigJson);
+            return currentExt;
+        }
+
+        private RNObject lookupExtension()
+        {
+            GenericObject generic = new GenericObject();
+            RNObjectType rnObj = new RNObjectType();
+            rnObj.Namespace = "SvcVentures";
+            rnObj.TypeName = "scProductExtension";
+            generic.ObjectType = rnObj;
+
+            string query = "SELECT SvcVentures.scProductExtension FROM SvcVentures.scProductExtension where Signature like '" + this.extSignature + "';";
+
+            RNObject[] objectTemplate = new RNObject[] { generic };
+            RNObject[] result = null;
+            try
+            {
+                QueryResultData[] queryObjects = client.QueryObjects(
+                    new ClientInfoHeader { AppID = "SCLog" },
+                    query,
+                    objectTemplate, 10000);
+                result = queryObjects[0].RNObjectsResult;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            if (result != null && result.Length > 0)
+            {
+                return result[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private RNObject createExtension()
+        {
+            GenericObject go = new GenericObject();
+
+            //Set the object type
+            RNObjectType objType = new RNObjectType();
+            objType.Namespace = "SvcVentures";
+            objType.TypeName = "scProductExtension";
+            go.ObjectType = objType;
+
+            List<GenericField> gfs = new List<GenericField>();
+
+            gfs.Add(createGenericField("ExtensionName", ItemsChoiceType.StringValue, this.extName));
+            gfs.Add(createGenericField("Signature", ItemsChoiceType.StringValue, this.extSignature));
+            gfs.Add(createGenericField("Description", ItemsChoiceType.StringValue, "Not Specified"));
+            gfs.Add(createGenericField("Authors", ItemsChoiceType.StringValue, "Not Specified"));
+            gfs.Add(createGenericField("ExtConfiguration", ItemsChoiceType.StringValue, DEFAULT_CONFIG));
+            go.GenericFields = gfs.ToArray();
+
+            CreateProcessingOptions cpo = new CreateProcessingOptions();
+            cpo.SuppressExternalEvents = false;
+            cpo.SuppressRules = false;
+
+            ClientInfoHeader clientInfoHeader = new ClientInfoHeader();
+            clientInfoHeader.AppID = "Create Extension";
+            RNObject[] results = client.Create(clientInfoHeader, new RNObject[] { go }, cpo);
+
+            // check result and save incident id
+            if (results != null && results.Length > 0)
+            {
+                return go;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private GenericField createGenericField(string Name, ItemsChoiceType itemsChoiceType, object Value)
+        {
+            GenericField gf = new GenericField();
+            gf.name = Name;
+            gf.DataValue = new DataValue();
+            gf.DataValue.ItemsElementName = new ItemsChoiceType[] { itemsChoiceType };
+            gf.DataValue.Items = new object[] { Value };
+            return gf;
+        }
+    }
+
+    public class extConfig
+    {
+        public Int32 logToDatabase { get; set; }
+        public Int32 logThreshhold { get; set; }
+        public Int32 logClicks { get; set; }
+        public Int32 logPessimistic { get; set; }
+        public Int32 logToFile { get; set; }
+    }
+}
